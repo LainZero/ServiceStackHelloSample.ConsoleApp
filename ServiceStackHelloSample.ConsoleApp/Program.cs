5fe7db90505faabe6c75193a19dc4d3b5b1ee10d
LainZero <lainzero@gmail.com>
2013-08-10 00:35:46 +0900

    Initial commit

---
+++ ServiceStackHelloSample.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ServiceStack.ServiceClient.Web;
+using ServiceStackHelloSample.ServiceModel.Operations;
+using ServiceStackHelloSample.ConsoleApp.ServiceStackHelloWebService;
+
+namespace ServiceStackHelloSample.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JsonServiceClient client = new JsonServiceClient("http://localhost:8080/");
+            HelloResponse response = client.Get<HelloResponse>("/hello");
+            Console.WriteLine(response.Result.ToString());
+            Console.ReadLine();
+        }
+    }
+}
